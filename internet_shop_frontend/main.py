b374835af425753cd034961bcd76ca4dd34237e0
Mykola322 <mikolasutij2009@gmail.com>
2025-04-02 19:08:17 +0300

    cuit

--- internet_shop_frontend/main.py
+++ internet_shop_frontend/main.py
@@ -1,15 +1,16 @@
 from flask import Flask, flash, render_template, redirect, url_for, request
 
 from src.data import data_actions
-from src.data.forms import ReviewForm, ProductForm
+from src.data.forms import ReviewForm
 
 
 app = Flask(__name__, template_folder="src/templates")
 app.secret_key = "aboba"
 
+
 @app.get("/")
 def index():
-    products = data_actions.get_product()
+    products = data_actions.get_products()
     return render_template("index.html", products=products)
 
 
@@ -27,18 +28,11 @@ def get_product(product_id):
     return render_template("product.html", product=product, form=form)
 
 
-@app.get("/buy_product/<product_id>/")
-def buy_product(product_id):
-    product = data_actions.get_product(product_id)
-    flash(f"Вітаємо з покупкою! '{product['name']}'", category="success")
-    return redirect(url_for("index"))
-
-
 @app.post("/buy_product/<product_id>/")
 def buy_product(product_id):
     name = request.form.get("name")
     data_actions.buy_product(product_id, name)
     return redirect(url_for("index"))
 
-if __name__ =="main":
+if __name__ =="__main__":
     app.run(debug=True)
--- internet_shop_frontend/src/data/forms.py
+++ internet_shop_frontend/src/data/forms.py
@@ -4,5 +4,5 @@ import wtforms
 
 class ReviewForm(FlaskForm):
     text = wtforms.StringField("Введіть свій відгук про даний товар", validators=[wtforms.validators.length(5, message = "Відгук не може містити менше 5-ти символів!")])
-    name = wtforms.StringField("Введіть своє ім'я", validators=[wtforms.validators.DataReqired()])
+    name = wtforms.StringField("Введіть своє ім'я", validators=[wtforms.validators.DataRequired()])
     submit = wtforms.SubmitField("Зберегти")
